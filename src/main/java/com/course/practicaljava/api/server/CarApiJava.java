08feff46c0657b8f783fd9a0c1df13949231b228
Harshita <harshita1h@gmail.com>
2021-06-05 13:54:00 +0530

    Added available, releaseDate and price attributes

--- src/main/java/com/course/practicaljava/api/server/CarApiJava.java
+++ src/main/java/com/course/practicaljava/api/server/CarApiJava.java
@@ -3,6 +3,8 @@ package com.course.practicaljava.api.server;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +21,10 @@ public class CarApiJava {
 	@GetMapping(value="/random", produces = MediaType.APPLICATION_JSON_VALUE)
 	public Car random() {
 		return carService.generateCar();
+	}
+	
+	@PostMapping(value="/echo",consumes = MediaType.APPLICATION_JSON_VALUE)
+	public String getCar(@RequestBody Car car) {
+		return car.toString();
 	}
 }
--- src/main/java/com/course/practicaljava/entity/Car.java
+++ src/main/java/com/course/practicaljava/entity/Car.java
@@ -1,14 +1,26 @@
 package com.course.practicaljava.entity;
 
+import java.time.LocalDate;
+
 public class Car {
-	public String brand;
-	public String color;
-	public String type;
+	private Boolean available;
+	private String brand;
+	private String color;
+	private LocalDate firstReleaseData;
+	private int price;
+	private String type;
+	
+	public Car() {
+		
+	}
 	public Car(String brand, String color, String type) {
 		super();
 		this.brand = brand;
 		this.color = color;
 		this.type = type;
+	}
+	public Boolean getAvailable() {
+		return available;
 	}
 	public String getBrand() {
 		return brand;
@@ -16,14 +28,29 @@ public class Car {
 	public String getColor() {
 		return color;
 	}
+	public LocalDate getFirstReleaseData() {
+		return firstReleaseData;
+	}
+	public int getPrice() {
+		return price;
+	}
 	public String getType() {
 		return type;
+	}
+	public void setAvailable(Boolean available) {
+		this.available = available;
 	}
 	public void setBrand(String brand) {
 		this.brand = brand;
 	}
 	public void setColor(String color) {
 		this.color = color;
+	}
+	public void setFirstReleaseData(LocalDate firstReleaseData) {
+		this.firstReleaseData = firstReleaseData;
+	}
+	public void setPrice(int price) {
+		this.price = price;
 	}
 	public void setType(String type) {
 		this.type = type;
